148c61a302aa2cf1bee435b12ca599e84b776490
vasarv <76836836+vasarv@users.noreply.github.com>
2022-03-23 16:20:25 +0300

    Update routersploiinstaller.sh

--- routersploiinstaller.sh
+++ routersploiinstaller.sh
@@ -1,8 +1,9 @@
 cd &&
 git clone https://github.com/threat9/routersploit &&
-cd routersploit/
+cd routersploit/ &&
 python3 -m pip install -r requirements.txt &&
 python3 -m pip install -r requirements.txt &&
+sudo echo 'cd &&' >> /usr/local/bin/routersploit &&
 sudo echo 'cd routersploit/ &&' >> /usr/local/bin/routersploit &&
 sudo echo 'python3 rsf.py' >> /usr/local/bin/routersploit &&
 echo "routersploit installed, to start routersploit enter command: 'routersploit'"
